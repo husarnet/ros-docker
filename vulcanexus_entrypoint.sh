18f8bc9428feb8526fe00f03862b312c19be6831
dominikn <dominik.nwk@gmail.com>
2023-02-28 09:12:14 +0100

    fix entrypoint

---
+++ vulcanexus_entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+output=$(husarnet-dds singleshot) || true
+if [[ "$HUSARNET_DDS_DEBUG" == "TRUE" ]]; then
+  echo "$output"
+fi
+
+# setup ros environment
+source "/opt/vulcanexus/$ROS_DISTRO/setup.bash"
+test -f "/ros2_ws/install/setup.bash" && source "/ros2_ws/install/setup.bash"
+
+exec "$@"
