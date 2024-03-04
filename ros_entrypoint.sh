0cfa2484d0a7ce59b17aa04a07828a941a941d62
dominikn <dominik.nwk@gmail.com>
2024-03-04 23:00:30 +0100

    using gosu only if user is different than root

--- ros_entrypoint.sh
+++ ros_entrypoint.sh
@@ -3,23 +3,26 @@ set -e
 
 output=$(husarnet-dds singleshot) || true
 if [[ "$HUSARNET_DDS_DEBUG" == "TRUE" ]]; then
-  echo "$output"
+    echo "$output"
 fi
 
 # setup ros environment
 source "/opt/ros/$ROS_DISTRO/setup.bash"
 test -f "/ros2_ws/install/setup.bash" && source "/ros2_ws/install/setup.bash"
 
-if [ -z "$USER" ]; then
-    export USER=root
-elif ! id "$USER" &>/dev/null; then
-    useradd -ms /bin/bash "$USER"
-fi
-
 # <additional-user-commands>
 
-if [ $# -eq 0 ]; then
-    exec gosu $USER /bin/bash
+if [ ! -z "$USER" ] && [ "$USER" != "root" ]; then
+    # Check if the user already exists; if not, create the user
+    if ! id "$USER" &>/dev/null; then
+        useradd -ms /bin/bash "$USER"
+    fi
+
+    if [ $# -eq 0 ]; then
+        exec gosu $USER /bin/bash
+    else
+        exec gosu $USER "$@"
+    fi
 else
-    exec gosu $USER "$@"
+    exec "$@"
 fi
